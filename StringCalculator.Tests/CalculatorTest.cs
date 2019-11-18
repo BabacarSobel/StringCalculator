1134976b85276219c27ea1d77c895fdd89dc1234
Babacar Sobel THIAW <bsthiaw@gmail.com>
2019-11-18 03:47:30 +0100

    Create projects for class lib and tests

---
+++ StringCalculator.Tests/CalculatorTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace StringCalculator.Tests
+{
+    public class CalculatorTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+    }
+}
